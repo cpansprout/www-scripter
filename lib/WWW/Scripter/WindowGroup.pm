6be8bf41b1a9d3db5b4fd606addf02a2c29ed8b1
sprout <sprout@Pint.local>
2016-07-17 11:13:59 -0700

    Release 0.032

--- lib/WWW/Scripter/WindowGroup.pm
+++ lib/WWW/Scripter/WindowGroup.pm
@@ -1,6 +1,6 @@
 package WWW::Scripter::WindowGroup;
 
-$VERSION = '0.031'; # update POD, too
+$VERSION = '0.032'; # update POD, too
 
 use Carp;
 use WWW'Scripter;
@@ -66,7 +66,7 @@ WWW::Scripter::WindowGroup - Multiple-window browsing environment
 
 =head1 VERSION
 
-Version 0.031
+Version 0.032
 
 =head1 SYNOPSIS
 
